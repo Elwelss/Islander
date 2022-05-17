1c467fab63e7aec51cf4fe028b73c4636a721939
Elwelss <jed.saliba@gmail.com>
2022-05-17 12:55:56 +0200

    Demo
    -Currently working on camera movement
    
    -Need to work on panel for items
    
    -Menu needs work

---
+++ Test maps/Assets/Scripts/CameraController.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float panSpeed = 20f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position; 
+
+        if (Input.GetKey("w"))
+        {
+            pos.z += panSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("s"))
+        {
+            pos.z -= panSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("a"))
+        {
+            pos.x -= panSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("d"))
+        {
+            pos.y += panSpeed * Time.deltaTime;
+        }
+
+        transform.position = pos;
+    }
+}
