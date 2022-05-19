1cdd373e79000df3ff46bee77203c4d563a879cd
Elwelss <jed.saliba@gmail.com>
2022-05-19 09:41:19 +0200

    V2.63819
    -Fixed and added more camera controlled (all ready)
    
    -added temporary menu button (red)
    
    -added temporary sidebar + sidebar_buttons

--- Test maps/Assets/Scripts/CameraController.cs
+++ Test maps/Assets/Scripts/CameraController.cs
@@ -3,6 +3,12 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public float panSpeed = 20f;
+    public float panBorderThickness = 10f;
+    public Vector2 panLimit;
+
+    public float scrollSpeed = 20f;
+    public float minY =  10f;
+    public float maxY = 110f;
 
     // Update is called once per frame
     void Update()
@@ -26,8 +32,15 @@ public class CameraController : MonoBehaviour
 
         if (Input.GetKey("d"))
         {
-            pos.y += panSpeed * Time.deltaTime;
+            pos.x += panSpeed * Time.deltaTime;
         }
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        pos.y += scroll * scrollSpeed * 100f * Time.deltaTime;
+
+        pos.x = Mathf.Clamp(pos.x, -panLimit.x, panLimit.x);
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+        pos.z = Mathf.Clamp(pos.z, -panLimit.y, panLimit.y);
 
         transform.position = pos;
     }
